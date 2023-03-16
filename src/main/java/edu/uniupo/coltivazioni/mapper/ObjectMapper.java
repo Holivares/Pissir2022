0d367d648b0bb62a2cfaddf16dfa66cb62788ce7
Holivares <58671689+Holivares@users.noreply.github.com>
2023-03-16 21:26:48 +0100

    mise à jour de ma branche

--- src/main/java/edu/uniupo/coltivazioni/mapper/ObjectMapper.java
+++ src/main/java/edu/uniupo/coltivazioni/mapper/ObjectMapper.java
@@ -12,34 +12,34 @@ import org.mapstruct.MappingConstants;
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public abstract class ObjectMapper {
     //Converti un dao en dto
-    abstract DTOAttuatore attuatoreToDTOAttuatore(Attuatore attuatore);
-    abstract DTOSerra serraToDTOSerra(Serra serra);
-    abstract DTOStatoAttuatore statoAttuatoreToDTOStatoAttuatore(StatoAttuatore statoAttuatore);
-    abstract DTOTipoAttuatore tipoAttuatoreToDTOTipoAttuatore(TipoAttuatore tipoAttuatore);
-    abstract DTOAttuatoreMode attuatoreModeTODTOAttuatoreMode(AttuatoreMode attuatoreMode);
-    abstract DTOAttuatoreStato attuatoreStatoTODTOAttuatoreStato(AttuatoreStato attuatoreStato);
-    abstract DTOUtente utenteTODTOUtente(Utente utente);
-    abstract DTOAziendaAgricola aziendaAgricolaTODTOAziendaAgricola(AziendaAgricola aziendaAgricola);
-    abstract DTOSensore sensoreTODTOSensore(Sensore sensore);
-    abstract DTOIrrigazionePianificatore irrigazionePianificatoreTODTOIrrigazionePianificatore(IrrigazionePianificatore irrigazionePianificatore);
-    abstract DTOMisura misuraTODTOMisura(Misura misura);
-    abstract DTORuolo ruoloTODTORuolo(Ruolo ruolo);
-    abstract DTOExecutionTime executionTimeTODTOExecutionTime(ExecutionTime executionTime);
+    public abstract DTOAttuatore attuatoreToDTOAttuatore(Attuatore attuatore);
+    public abstract DTOSerra serraToDTOSerra(Serra serra);
+    public abstract DTOStatoAttuatore statoAttuatoreToDTOStatoAttuatore(StatoAttuatore statoAttuatore);
+    public abstract DTOTipoAttuatore tipoAttuatoreToDTOTipoAttuatore(TipoAttuatore tipoAttuatore);
+    public abstract DTOAttuatoreMode attuatoreModeTODTOAttuatoreMode(AttuatoreMode attuatoreMode);
+    public abstract DTOAttuatoreStato attuatoreStatoTODTOAttuatoreStato(AttuatoreStato attuatoreStato);
+    public abstract DTOUtente utenteTODTOUtente(Utente utente);
+    public abstract DTOAziendaAgricola aziendaAgricolaTODTOAziendaAgricola(AziendaAgricola aziendaAgricola);
+    public abstract DTOSensore sensoreTODTOSensore(Sensore sensore);
+    public abstract DTOIrrigazionePianificatore irrigazionePianificatoreTODTOIrrigazionePianificatore(IrrigazionePianificatore irrigazionePianificatore);
+    public abstract DTOMisura misuraTODTOMisura(Misura misura);
+    public abstract DTORuolo ruoloTODTORuolo(Ruolo ruolo);
+    public abstract DTOExecutionTime executionTimeTODTOExecutionTime(ExecutionTime executionTime);
 
     //converti dto en dao
 
-    abstract Attuatore dTOAttuatoreToAttuatore(DTOAttuatore dtoAttuatore);
-    abstract DTOSerra dTOSerraToSerra(DTOSerra DtoSerra);
-    abstract DTOStatoAttuatore dTOStatoAttuatoreToStatoAttuatore(DTOStatoAttuatore dtoStatoAttuatore);
-    abstract DTOTipoAttuatore dTOTipoAttuatoreToTipoAttuatore(DTOTipoAttuatore dtoTipoAttuatore);
-    abstract DTOAttuatoreMode dTOAttuatoreModeTOAttuatoreMode(DTOAttuatoreMode dtoAttuatoreMode);
-    abstract DTOAttuatoreStato dTOAttuatoreStatoTOAttuatoreStato(DTOAttuatoreStato dtoAttuatoreStato);
-    abstract DTOUtente dTOUtenteTOUtente(DTOUtente dtoUtente);
-    abstract DTOAziendaAgricola dTOAziendaAgricolaTOAziendaAgricola(DTOAziendaAgricola dtoAziendaAgricola);
-    abstract DTOSensore dTOSensoreTOSensore(DTOSensore dtoSensore);
-    abstract DTOIrrigazionePianificatore dTOIrrigazionePianificatoreTOIrrigazionePianificatore(DTOIrrigazionePianificatore dtoIrrigazionePianificatore);
-    abstract DTOMisura dTOMisuraTOMisura(DTOMisura dtoMisura);
-    abstract DTORuolo dTORuoloTORuolo(DTORuolo dtoRuolo);
-    abstract DTOExecutionTime dTOExecutionTimeTOExecutionTime(DTOExecutionTime dtoExecutionTime);
+    public abstract Attuatore dTOAttuatoreToAttuatore(DTOAttuatore dtoAttuatore);
+    public abstract DTOSerra dTOSerraToSerra(DTOSerra DtoSerra);
+    public abstract DTOStatoAttuatore dTOStatoAttuatoreToStatoAttuatore(DTOStatoAttuatore dtoStatoAttuatore);
+    public abstract DTOTipoAttuatore dTOTipoAttuatoreToTipoAttuatore(DTOTipoAttuatore dtoTipoAttuatore);
+    public abstract DTOAttuatoreMode dTOAttuatoreModeTOAttuatoreMode(DTOAttuatoreMode dtoAttuatoreMode);
+    public abstract DTOAttuatoreStato dTOAttuatoreStatoTOAttuatoreStato(DTOAttuatoreStato dtoAttuatoreStato);
+    public abstract DTOUtente dTOUtenteTOUtente(DTOUtente dtoUtente);
+    public abstract DTOAziendaAgricola dTOAziendaAgricolaTOAziendaAgricola(DTOAziendaAgricola dtoAziendaAgricola);
+    public abstract DTOSensore dTOSensoreTOSensore(DTOSensore dtoSensore);
+    public abstract DTOIrrigazionePianificatore dTOIrrigazionePianificatoreTOIrrigazionePianificatore(DTOIrrigazionePianificatore dtoIrrigazionePianificatore);
+    public abstract DTOMisura dTOMisuraTOMisura(DTOMisura dtoMisura);
+    public abstract DTORuolo dTORuoloTORuolo(DTORuolo dtoRuolo);
+    public abstract DTOExecutionTime dTOExecutionTimeTOExecutionTime(DTOExecutionTime dtoExecutionTime);
 
 }
